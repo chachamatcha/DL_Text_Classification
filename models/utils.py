a034784164a0c1c669b64a3014d6da8b0740f4e4
Ubuntu <ubuntu@ip-172-31-11-142.us-west-2.compute.internal>
2018-04-14 19:57:42 +0000

    structure, initial notebooks, and assets...

---
+++ models/utils.py
@@ -0,0 +1,88 @@
+import numpy as np
+from tqdm import tqdm # pbar
+from keras.preprocessing import text, sequence # text tools
+from keras.callbacks import Callback # Callbacks
+from sklearn.metrics import roc_auc_score # roc from sklearn
+
+
+class RocAucEvaluation(Callback):
+    def __init__(self, validation_data=(), interval=1):
+        super(Callback, self).__init__()
+
+        self.interval = interval
+        self.X_val, self.y_val = validation_data
+
+    def on_epoch_end(self, epoch, logs={}):
+        if epoch % self.interval == 0:
+            y_pred = self.model.predict(self.X_val, verbose=0)
+            score = roc_auc_score(self.y_val, y_pred)
+            logs['roc_val'] = score
+            print("\n ROC-AUC - epoch: %d - score: %.6f \n" % (epoch+1, score))
+            
+
+
+def tok_embed(X_train, X_test, 
+              embedding_path='../data/glove.42B.300d.txt', 
+              max_features = 100000, 
+              maxlen = 200, 
+              embed_size = 300, 
+              verbose=True):
+    
+    if verbose:
+        print('tokenizing...')
+    
+    # tokenize
+    tokenizer = text.Tokenizer(num_words=max_features)
+    tokenizer.fit_on_texts(list(X_train) + list(X_test))
+    X_train = tokenizer.texts_to_sequences(X_train)
+    X_test = tokenizer.texts_to_sequences(X_test)
+    
+    # pad
+    x_train = sequence.pad_sequences(X_train, maxlen=maxlen)
+    x_test = sequence.pad_sequences(X_test, maxlen=maxlen)
+    
+    # get word vectors
+    embeddings_index = {}
+    f = open(embedding_path)
+    
+    # coefs
+    if verbose:
+        for line in tqdm(f, desc='getting coefs'):
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs
+    else:
+        for line in f:
+            values = line.split()
+            word = values[0]
+            coefs = np.asarray(values[1:], dtype='float32')
+            embeddings_index[word] = coefs                     
+    f.close()
+
+    word_index = tokenizer.word_index
+
+    # init embedding matrix
+    num_words = min(max_features, len(word_index) + 1)
+    embedding_matrix = np.zeros((num_words, embed_size))
+    
+    if verbose:        
+        for word, i in tqdm(word_index.items(),desc='creating embedding matrix'):
+            embedding_vector = embeddings_index.get(word)
+            if i >= max_features: continue
+            if embedding_vector is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = embedding_vector
+    else:
+        for word, i in word_index.items():
+            embedding_vector = embeddings_index.get(word)
+            if i >= max_features: continue
+            if embedding_vector is not None:
+                # words not found in embedding index will be all-zeros.
+                embedding_matrix[i] = embedding_vector
+                
+    # print found word vectors
+    if verbose:
+        print('Found %s word vectors.' % len(embeddings_index))
+
+    return x_train, x_test, embedding_matrix
